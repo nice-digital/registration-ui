cff0dfc044b38c89cc424b2b00649c87896b311d
John Holland <john.holland@nice.org.uk>
2021-08-17 18:22:12 +0100

    Added sass, global nav and nds
    Also updated config to allow nds components to work properly.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-module.exports = {
+const { readdirSync } = require("fs"),
+	path = require("path");
+	withTranspiledModules = require("next-transpile-modules");
+
+/**
+ * A list of paths to node modules that should allow transpilation.
+ * Most of our Design System components (and Global Nav) import SCSS.
+ *
+ * Avoids the error "CSS Modules cannot be imported from within node_modules."
+ */
+const niceDigitalModulesToTranspile = readdirSync(
+	path.join(__dirname, "node_modules", "@nice-digital"),
+	{ withFileTypes: true }
+)
+	.filter((dirent) => dirent.isDirectory())
+	.map(({ name }) => `@nice-digital/${name}`);
+
+const nextConfig = {
   reactStrictMode: true,
-  webpack5: false,
   env: {
     AUTH0_SECRET: process.env.AUTH0_SECRET,
     AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
@@ -11,4 +27,8 @@ module.exports = {
     AUTH0_POST_LOGOUT_REDIRECT: process.env.AUTH0_POST_LOGOUT_REDIRECT,
     APIGatewayAddress: process.env.APIGatewayAddress
   }
-}
+};
+
+const finalConfig = withTranspiledModules(niceDigitalModulesToTranspile)(nextConfig);
+
+module.exports = finalConfig;
