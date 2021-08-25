2c4e7ce3c832cca7139aa7cc25517503e5898a79
John Holland <john.holland@nice.org.uk>
2021-08-25 14:36:10 +0100

    Added loggedOut option for layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,8 @@
 import { Header, Footer, IdamProviderProps } from "@nice-digital/global-nav";
 import { Container } from "@nice-digital/nds-container";
 
-export default function Layout({ children }: any) {
-    const auth: IdamProviderProps = {
+export default function Layout({ children, loggedOut }: any) {
+	const auth: IdamProviderProps = {
 		links: [
 			{ text: "My registrations", url: "/registrations" },
 			{ text: "Sign out", url: "/api/auth/logout" },
@@ -11,13 +11,18 @@ export default function Layout({ children }: any) {
 		provider: "idam",
 	};
 
-  return (
-    <>
-      <Header search={false} auth={auth} />    
-      <Container>
-          {children}
-      </Container>
-      <Footer />
-    </>
-  );
+	if (loggedOut) {
+		auth.links = [{ text: "Sign in", url: "/api/auth/login" }];
+		auth.displayName = "";
+	}
+
+	return (
+		<>
+			<Header search={false} auth={auth} />    
+			<Container>
+				{children}
+			</Container>
+			<Footer />
+		</>
+	);
 }
