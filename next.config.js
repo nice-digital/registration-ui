c6cf8fe8c287efa4e5a4c47f0ce0e08966a62224
John Holland <john.holland@nice.org.uk>
2021-10-05 17:41:19 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -25,6 +25,7 @@ const nextConfig = {
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
     AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
     AUTH0_POST_LOGOUT_REDIRECT: process.env.AUTH0_POST_LOGOUT_REDIRECT,
+    SELF_URL: process.env.SELF_URL,
     BACKEND_URL: process.env.BACKEND_URL,
     INDEV_URL: process.env.INDEV_URL
   }
