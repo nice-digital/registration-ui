b238e5a4b59ff858a962e3965f6a3d6b1e491f53
Cheguegallagher <martin.gallagher@nice.org.uk>
2021-10-13 18:00:41 +0100

    Passing test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,8 +1,13 @@
 describe('Navigate to RegUi homepage', () => {
   it('Finds the Register as Stakeholder link', () => {
-    cy.visit('http://localhost:3000/');
+    cy.clearCookies();
+    cy.visit('/');
     cy.get('a').contains('Register as a stakeholder').click();
     cy.url().should('include', 'alpha-identity.nice.org.uk/login');
     cy.login();
+    cy.wait(500);
+    cy.reload();
+    cy.get('h1').contains('Profile builder');
+    cy.get('.checkbox__input').should('have.attr', 'checked');
   });
 });
